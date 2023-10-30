779bd296d84d177a8b73f377641ac2e2fb707dc1
fcaballaro <fcaballaro@jusentrerios.gov.ar>
2023-10-30 12:46:12 -0300

    Updates

---
+++ precache-manifest.293d27a5150d72450ee468867504e1d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560603d5bb989c9efda30c277902f1b6",
+    "url": "/sorteo-simple/index.html"
+  },
+  {
+    "revision": "2debf011e01d7421c836",
+    "url": "/sorteo-simple/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4716b8eec49ba385d40c",
+    "url": "/sorteo-simple/static/js/2.3089d221.chunk.js"
+  },
+  {
+    "revision": "7a870419af2a9bfab083c177351477fd",
+    "url": "/sorteo-simple/static/js/2.3089d221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e56bc22558771c1b0e",
+    "url": "/sorteo-simple/static/js/3.1a843487.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/sorteo-simple/static/js/3.1a843487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0225854313b6538f0131",
+    "url": "/sorteo-simple/static/js/4.6409bc3e.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/sorteo-simple/static/js/4.6409bc3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695edcc5036762a2f46e",
+    "url": "/sorteo-simple/static/js/5.4023de06.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/sorteo-simple/static/js/5.4023de06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2debf011e01d7421c836",
+    "url": "/sorteo-simple/static/js/main.dd5cfa44.chunk.js"
+  },
+  {
+    "revision": "38c5efaa9d89d2486d50",
+    "url": "/sorteo-simple/static/js/runtime-main.61e3c3b1.js"
+  }
+]);
